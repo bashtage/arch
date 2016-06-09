33de173d7c028c66b9be0124c7624e39bba3451a
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2016-06-09 23:11:11 +0200

    TST: Add test against statsmodels master
    Add a test against statsmodels master to prevent surprises

---
+++ ci/statsmodels-master.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+conda remove statsmodels --yes
+export GITDIR=$PWD
+cd ~
+git clone --branch=master --depth=10 https://github.com/statsmodels/statsmodels.git
+cd statsmodels
+python setup.py install
+cd $GITDIR
