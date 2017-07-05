66dd1a7c88608c7541343a83b7bd8783d22086e3
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2017-07-05 11:24:19 +0100

    ENH: Improve KPSS crit val simulation
    Improve simulation to improve resolution for small pvals
    Update so that it works in current python
    Fix small LGTM issue with bootstrap init

--- arch/bootstrap/__init__.py
+++ arch/bootstrap/__init__.py
@@ -1,9 +1,22 @@
+"""
+Tools for implementing statistical bootstraps
+"""
 from __future__ import absolute_import
 
+from arch.bootstrap import _samplers_python
 from arch.bootstrap.base import IIDBootstrap, CircularBlockBootstrap, MovingBlockBootstrap, \
     StationaryBootstrap
 from arch.bootstrap.multiple_comparrison import SPA, RealityCheck, StepM, MCS
+COMPILED_SAMPLERS = True
+try:
+    from arch.bootstrap import _samplers
+except ImportError:
+    COMPILED_SAMPLERS = False
+
 
 __all__ = ['IIDBootstrap', 'CircularBlockBootstrap', 'MovingBlockBootstrap',
            'StationaryBootstrap', 'SPA', 'RealityCheck', 'StepM', 'MCS',
-           '_samplers_python', '_samplers']
+           '_samplers_python']
+
+if COMPILED_SAMPLERS:
+    __all__ += ['_samplers']
