89afd218544f0b74fe7d436486ba7281fae23fdb
Travis CI User <travis@example.org>
2018-03-13 16:39:28 +0000

    Update docs after building Travis build 1060 of bashtage/arch
    The docs were built from the branch 'master' against the commit
    682427d18c4761bb833b6ed3520376284dfd9d66.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/bashtage/arch/jobs/352924697.
    
    The doctr command that was run is
    
        /home/travis/miniconda3/envs/arch-test/bin/doctr deploy doc --build-tags

--- doc/_static/documentation_options.js
+++ doc/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: '4.3.1',
+    VERSION: '4.3.1+1.g682427d',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
