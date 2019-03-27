59ead55db909f72234e6a439c2c7912fd0d7fac1
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2019-03-27 12:54:26 +0000

    BLD: Use pyargs to test
    Enable pytest to be run from installed package using a .test() function

--- arch/__init__.py
+++ arch/__init__.py
@@ -1,5 +1,6 @@
 from arch._version import get_versions
 from arch.univariate.mean import arch_model
+from arch.utility import test
 
 __version__ = get_versions()['version']
 del get_versions
@@ -10,4 +11,4 @@ def doc():
     webbrowser.open('http://arch.readthedocs.org/en/latest/')
 
 
-__all__ = ['arch_model', '__version__', 'doc']
+__all__ = ['arch_model', '__version__', 'doc', 'test']
