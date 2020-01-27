1e1fc33d1e460e3c88b010014ba267b72c25807d
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-01-27 19:26:22 +0000

    Update docs after building Travis build 1691 of bashtage/arch
    The docs were built from the branch 'master' against the commit
    f486691455f499a77307ec682c47234c687622c0.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/bashtage/arch/jobs/642535651.
    
    The doctr command that was run is
    
        /home/travis/miniconda3/envs/arch-test/bin/doctr deploy doc --build-tags

--- doc/_static/documentation_options.js
+++ doc/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.11+18.gf176217',
+    VERSION: '4.11+20.gf486691',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
