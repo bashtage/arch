f47fa6dce99952f076e7e7767a8375c66df40a63
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2020-12-17 13:42:21 +0000

    DOC: Update README
    Update with Azure CI badge

--- ci/install-common.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-python -m pip install property_cached flake8 pytest pytest-xdist pytest-cov coverage codacy-coverage coveralls codecov nbformat nbconvert jupyter_client ipython jupyter
-if [[ "$DOCBUILD" == true ]]; then
-  sudo apt-get install -y enchant
-  python -m pip install sphinx ipython numpydoc jupyter seaborn doctr nbsphinx sphinx_material sphinxcontrib-spelling sphinx-autodoc-typehints
-fi
