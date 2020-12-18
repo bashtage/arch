249a81db87d79e4b73f5f8c461d2cc07a19d06a8
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2020-12-18 15:01:21 +0000

    Add coverage reporting

--- arch/utility/__init__.py
+++ arch/utility/__init__.py
@@ -1,6 +1,5 @@
 import os
-import sys
-from typing import List, Optional, Sequence, Union
+from typing import List, Optional, Union
 
 from arch.utility.cov import cov_nw
 
@@ -8,8 +7,11 @@ PKG = os.path.dirname(os.path.dirname(__file__))
 
 
 def test(
-    extra_args: Optional[Union[str, Sequence[str]]] = None, exit: bool = True
-) -> None:
+    extra_args: Optional[Union[str, List[str]]] = None,
+    exit: bool = True,
+    append: bool = True,
+    location: str = "",
+) -> int:
     """
     Test runner that allows testing of installed package.
 
@@ -22,26 +24,36 @@ def test(
         and --disable-pytest-warnings. Providing extra_args overwrites the
         defaults with the user-provided arguments.
     """
+
+    import sys
+
     try:
         import pytest
     except ImportError:
         raise ImportError("Need pytest to run tests")
-    cmd: List[str] = []
-    default_args = ["--tb=short", "--disable-pytest-warnings"]
-    if extra_args is None:
-        extra_args_lst = default_args
-    elif isinstance(extra_args, str):
-        extra_args_lst = [extra_args]
-    else:  # extra_args is not None
-        extra_args_lst = list(extra_args)
 
-    if extra_args_lst:
-        cmd = extra_args_lst[:]
-    cmd = [PKG] + cmd
+    cmd = ["--tb=auto", "--disable-pytest-warnings"]
+    if extra_args:
+        if not isinstance(extra_args, list):
+            pytest_args = [extra_args]
+        else:
+            pytest_args = extra_args
+        if append:
+            cmd += pytest_args[:]
+        else:
+            cmd = pytest_args
+    pkg_loc = PKG
+    if location:
+        pkg_loc = os.path.abspath(os.path.join(PKG, location))
+    print(pkg_loc)
+    if not os.path.exists(pkg_loc):
+        raise RuntimeError(f"{pkg_loc} was not found. Unable to run tests")
+    cmd = [pkg_loc] + cmd
     print("running: pytest {}".format(" ".join(cmd)))
     status = pytest.main(cmd)
     if exit:
         sys.exit(status)
+    return status
 
 
 __all__ = ["cov_nw", "test"]
