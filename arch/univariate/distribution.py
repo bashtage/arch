f7896bf817e43e3de6401d36f3c08875fee8ce6e
Kevin Sheppard <bashtage@users.noreply.github.com>
2025-07-16 13:39:07 +0100

    TYP: Typing improvements

--- arch/univariate/distribution.py
+++ arch/univariate/distribution.py
@@ -5,7 +5,7 @@ Distributions to use in ARCH models.  All distributions must inherit from
 
 from abc import ABCMeta, abstractmethod
 from collections.abc import Sequence
-from typing import Callable, Optional, Union
+from typing import Callable, Optional, Union, cast
 import warnings
 
 import numpy as np
@@ -625,7 +625,7 @@ class StudentsT(Distribution, metaclass=AbstractDocStringInheritor):
         Uses relationship between kurtosis and degree of freedom parameter to
         produce a moment-based estimator for the starting values.
         """
-        k = stats.kurtosis(std_resid, fisher=False)
+        k = cast(float, stats.kurtosis(std_resid, fisher=False))
         sv = max((4.0 * k - 6.0) / (k - 3.0) if k > 3.75 else 12.0, 4.0)
         return array([sv])
 
@@ -886,7 +886,7 @@ class SkewStudent(Distribution, metaclass=AbstractDocStringInheritor):
         Uses relationship between kurtosis and degree of freedom parameter to
         produce a moment-based estimator for the starting values.
         """
-        k = stats.kurtosis(std_resid, fisher=False)
+        k = cast(float, stats.kurtosis(std_resid, fisher=False))
         sv = max((4.0 * k - 6.0) / (k - 3.0) if k > 3.75 else 12.0, 4.0)
         return array([sv, 0.0])
 
