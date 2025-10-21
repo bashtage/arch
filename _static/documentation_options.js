9beedb7f680f97f88efc8da703a27dbcaa7ec7fc
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2025-10-21 08:41:03 +0000

    Github Actions doc build after commit 038d78b7

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '7.2.0',
+    VERSION: '8.0.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- devel/_static/documentation_options.js
+++ devel/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: 'v7.2.1 (+134)',
+    VERSION: '8.0.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
