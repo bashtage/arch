efb475d352694796b0a28e3da084c68fd2424ecf
samanvirajput <samanvi.rajput5@gmail.com>
2026-06-12 00:05:19 +0530

    FMT: fix formatting

--- arch/tests/univariate/test_base.py
+++ arch/tests/univariate/test_base.py
@@ -3,6 +3,7 @@ from arch import arch_model
 import pandas as pd
 import numpy as np
 
+
 def test_format_float_fixed():
     out = format_float_fixed(0.0)
     assert out == "0.0000"
@@ -11,11 +12,12 @@ def test_format_float_fixed():
     out = format_float_fixed(123456789.0)
     assert out == "1.2346e+08"
 
+
 def test_std_resid_numpy_input_returns_series():
     np.random.seed(42)
     data = np.random.randn(500)
-    am = arch_model(data, vol='GARCH', p=1, q=1)
-    res = am.fit(disp='off')
+    am = arch_model(data, vol="GARCH", p=1, q=1)
+    res = am.fit(disp="off")
     sr = res.std_resid
     assert isinstance(sr, pd.Series)
     assert sr.name == "std_resid"
@@ -25,8 +27,8 @@ def test_std_resid_numpy_input_returns_series():
 def test_std_resid_pandas_input_still_works():
     np.random.seed(42)
     data = pd.Series(np.random.randn(500), name="returns")
-    am = arch_model(data, vol='GARCH', p=1, q=1)
-    res = am.fit(disp='off')
+    am = arch_model(data, vol="GARCH", p=1, q=1)
+    res = am.fit(disp="off")
     sr = res.std_resid
     assert isinstance(sr, pd.Series)
     assert sr.name == "std_resid"
@@ -35,8 +37,8 @@ def test_std_resid_pandas_input_still_works():
 def test_std_resid_numpy_has_index():
     np.random.seed(42)
     data = np.random.randn(500)
-    am = arch_model(data, vol='GARCH', p=1, q=1)
-    res = am.fit(disp='off')
+    am = arch_model(data, vol="GARCH", p=1, q=1)
+    res = am.fit(disp="off")
     sr = res.std_resid
     assert sr.index is not None
     assert len(sr.index) == 500
